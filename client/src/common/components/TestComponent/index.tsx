ee24208b29b94595cddeaf1a9d05610ad136b382
xiaoyu <xiaoyu@sulg.top>
2026-09-08 18:10:27 +0800

    feat: 简化测试组件

--- client/src/common/components/TestComponent/index.tsx
+++ client/src/common/components/TestComponent/index.tsx
@@ -1,6 +1,3 @@
-import React from 'react';
-import styles from './index.module.less';
-
 export default function TestComponent() {
-  return <div className={styles['test-component']}>TestComponent</div>;
+  return <div>TestComponent</div>;
 }
